9bf54c65d02be2991fd821daeb5377e1e0e5265c
Richard Kovalsky <kvl.richard@gmail.com>
2026-04-22 09:04:04 +0200

    Selection sort

--- sorting.py
+++ sorting.py
@@ -4,12 +4,23 @@ def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
 values = random_numbers(10)  # 10 čísel v rozsahu 0–100
-print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
-
 small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
+
+
+def selection_sort(values):
+
+    for x in range(len(values)):
+        min_idx = x
+        min_value = values[min_idx]
+        for i in range(x,len(values)):
+            if values[i] < min_value:
+                min_idx = i
+                min_value = values[i]
+        values[x],values[min_idx] = values[min_idx], values[x]
+        print(values)
+
 
 if __name__ == "__main__":
     values = random_numbers(10)
-    print(values)
     small = random_numbers(5,0,20)
-    print(small)
+    selection_sort(values)
